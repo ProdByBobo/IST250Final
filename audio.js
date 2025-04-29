c17df1cc1346b3a1eb9929e6a08abbef32caae5c
ProdByBobo <ama8745@psu.edu>
2025-04-29 12:25:27 -0400

    added space bar pause to audio

--- audio.js
+++ audio.js
@@ -21,14 +21,37 @@ albumImages.forEach(image => {
         audioPlayer.src = audioSrc;
         audioPlayer.play();
 
-        // Remove "Now Playing" overlay from the previous album
+        // Remove "Now Playing" and "Paused" overlay from the previous album
         if (currentPlaying) {
             currentPlaying.classList.remove('now-playing');
+            currentPlaying.classList.remove('paused');
         }
 
         // Add "Now Playing" overlay to the clicked album
         const card = image.closest('.card');
         card.classList.add('now-playing');
         currentPlaying = card;
+
+        
+        
+        
     });
+    
+});
+//when click spcacebar, pause or play the audio and add paused class to the card
+document.addEventListener('keydown', (event) => {
+    if (event.code === 'Space') {
+        event.preventDefault();
+        if (audioPlayer.paused) {
+            audioPlayer.play();
+            if (currentPlaying) {
+                currentPlaying.classList.remove('paused');
+            }
+        } else {
+            audioPlayer.pause();
+            if (currentPlaying) {
+                currentPlaying.classList.add('paused');
+            }
+        }
+    }
 });
